d27b747f5a85c628b00eb63e26b9a54405ab20a1
Ernesto Deluquez Heilbron <edeluquez@hotmail.com>
2025-08-29 03:47:34 +0000

    ⚡<fe0f> Improve perfomance and hadening security

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,22 +12,22 @@ for _ARG in "$@"; do
 
   case "$ARG" in
     doh-server=*)
-      awk -v doh="${ARG#*=}" '/server_names =/{print "server_names = [\x27" doh "\x27]";next}{print}' /run/conf/dnscrypt-proxy.toml >.tmp && mv .tmp /run/conf/dnscrypt-proxy.toml
+      awk -v doh="${ARG#*=}" '/server_names =/{print "server_names = [\x27" doh "\x27]";next}{print}' /run/conf/dnscrypt-proxy.toml >/run/conf/.tmp && mv /run/conf/.tmp /run/conf/dnscrypt-proxy.toml
       ;;
     nocache)
-      awk '/^cache-size/{ print "no-negcache\ncache-size=0";next }{print}' /run/conf/dnsmasq.conf >.tmp && mv .tmp /run/conf/dnsmasq.conf
+      awk '/^cache-size/{ print "no-negcache\ncache-size=0";next }{print}' /run/conf/dnsmasq.conf >/run/conf/.tmp && mv /run/conf/.tmp /run/conf/dnsmasq.conf
       ;;
     cachesize=*)
-      awk -v size="${ARG#*=}" '/^cache-size/{ $0 = "cache-size=" size }{print}' /run/conf/dnsmasq.conf >.tmp && mv .tmp /run/conf/dnsmasq.conf
+      awk -v size="${ARG#*=}" '/^cache-size/{ $0 = "cache-size=" size }{print}' /run/conf/dnsmasq.conf >/run/conf/.tmp && mv /run/conf/.tmp /run/conf/dnsmasq.conf
       ;;
     server=*)
       echo $ARG >>/run/conf/dnsmasq.conf
       ;;
     anonymized)
-      awk '/^\[sources\]/{print "[anonymized_dns]";print "routes = [";print "  { server_name='\''*'\'', via=['\''anon-relay-ams'\'', '\''anon-relay-par'\'', '\''anon-cs-md'\'', '\''anon-cs-fi'\'', '\''anon-cs-nl'\'', '\''anon-cs-pt'\'', '\''anon-ibksturm'\'', '\''anon-kama'\''] }";print "]\n";print "[sources]";next}{ print }' /run/conf/dnscrypt-proxy.toml >.tmp && mv .tmp /run/conf/dnscrypt-proxy.toml
+      awk '/^\[sources\]/{print "[anonymized_dns]";print "routes = [";print "  { server_name='\''*'\'', via=['\''anon-relay-ams'\'', '\''anon-relay-par'\'', '\''anon-cs-md'\'', '\''anon-cs-fi'\'', '\''anon-cs-nl'\'', '\''anon-cs-pt'\'', '\''anon-ibksturm'\'', '\''anon-kama'\''] }";print "]\n";print "[sources]";next}{ print }' /run/conf/dnscrypt-proxy.toml >/run/conf/.tmp && mv /run/conf/.tmp /run/conf/dnscrypt-proxy.toml
       ;;
     doh-route=*)
-      awk -v route="${ARG#*=}" '{sub(/server_name='\''[^'\'']+'\''/, "server_name='\''" route "'\''");print}' /run/conf/dnscrypt-proxy.toml >.tmp && mv .tmp /run/conf/dnscrypt-proxy.toml
+      awk -v route="${ARG#*=}" '{sub(/server_name='\''[^'\'']+'\''/, "server_name='\''" route "'\''");print}' /run/conf/dnscrypt-proxy.toml >/run/conf/.tmp && mv /run/conf/.tmp /run/conf/dnscrypt-proxy.toml
       ;;
   esac
 done
