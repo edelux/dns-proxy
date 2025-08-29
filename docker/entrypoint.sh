8dd2c64a35e4a4067e6e928f2a5b76631c65cd5d
Ernesto Deluquez Heilbron <edeluquez@hotmail.com>
2025-08-29 02:17:57 +0000

    ⚡️ Improve perfomance and hadening security

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir -p /run/conf
+cp /etc/dnsmasq.conf /etc/dnscrypt-proxy/dnscrypt-proxy.toml /run/conf
+
 for _ARG in "$@"; do
   case "$_ARG" in
     --*) ARG="${_ARG#--}" ;;
@@ -9,25 +12,25 @@ for _ARG in "$@"; do
 
   case "$ARG" in
     doh-server=*)
-      awk -v doh="${ARG#*=}" '/server_names =/{print "server_names = [\x27" doh "\x27]";next}{print}' /etc/dnscrypt-proxy/dnscrypt-proxy.toml >.tmp && mv .tmp /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+      awk -v doh="${ARG#*=}" '/server_names =/{print "server_names = [\x27" doh "\x27]";next}{print}' /run/conf/dnscrypt-proxy.toml >.tmp && mv .tmp /run/conf/dnscrypt-proxy.toml
       ;;
     nocache)
-      awk '/^cache-size/{ print "no-negcache\ncache-size=0";next }{print}' /etc/dnsmasq.conf >.tmp && mv .tmp /etc/dnsmasq.conf
+      awk '/^cache-size/{ print "no-negcache\ncache-size=0";next }{print}' /run/conf/dnsmasq.conf >.tmp && mv .tmp /run/conf/dnsmasq.conf
       ;;
     cachesize=*)
-      awk -v size="${ARG#*=}" '/^cache-size/{ $0 = "cache-size=" size }{print}' /etc/dnsmasq.conf >.tmp && mv .tmp /etc/dnsmasq.conf
+      awk -v size="${ARG#*=}" '/^cache-size/{ $0 = "cache-size=" size }{print}' /run/conf/dnsmasq.conf >.tmp && mv .tmp /run/conf/dnsmasq.conf
       ;;
     server=*)
-      echo $ARG >> /etc/dnsmasq.conf
+      echo $ARG >>/run/conf/dnsmasq.conf
       ;;
     anonymized)
-      awk '/^\[sources\]/{print "[anonymized_dns]";print "routes = [";print "  { server_name='\''*'\'', via=['\''anon-relay-ams'\'', '\''anon-relay-par'\'', '\''anon-cs-md'\'', '\''anon-cs-fi'\'', '\''anon-cs-nl'\'', '\''anon-cs-pt'\'', '\''anon-ibksturm'\'', '\''anon-kama'\''] }";print "]\n";print "[sources]";next}{ print }' /etc/dnscrypt-proxy/dnscrypt-proxy.toml >.tmp && mv .tmp /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-      ;; 
+      awk '/^\[sources\]/{print "[anonymized_dns]";print "routes = [";print "  { server_name='\''*'\'', via=['\''anon-relay-ams'\'', '\''anon-relay-par'\'', '\''anon-cs-md'\'', '\''anon-cs-fi'\'', '\''anon-cs-nl'\'', '\''anon-cs-pt'\'', '\''anon-ibksturm'\'', '\''anon-kama'\''] }";print "]\n";print "[sources]";next}{ print }' /run/conf/dnscrypt-proxy.toml >.tmp && mv .tmp /run/conf/dnscrypt-proxy.toml
+      ;;
     doh-route=*)
-      awk -v route="${ARG#*=}" '{sub(/server_name='\''[^'\'']+'\''/, "server_name='\''" route "'\''");print}' /etc/dnscrypt-proxy/dnscrypt-proxy.toml >.tmp && mv .tmp /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+      awk -v route="${ARG#*=}" '{sub(/server_name='\''[^'\'']+'\''/, "server_name='\''" route "'\''");print}' /run/conf/dnscrypt-proxy.toml >.tmp && mv .tmp /run/conf/dnscrypt-proxy.toml
       ;;
   esac
 done
 
-dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml &
-dnsmasq --conf-file=/etc/dnsmasq.conf
+dnscrypt-proxy -config /run/conf/dnscrypt-proxy.toml &
+dnsmasq --conf-file=/run/conf/dnsmasq.conf
